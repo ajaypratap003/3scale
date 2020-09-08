e383feca6c94a3a27a75a2d56e9abdd2ed3cf064
redallen <zallen@redhat.com>
2020-09-08 11:43:34 -0400

    don't cache js files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ModuleFederationPlugin = require("webpack").container.ModuleFederationPlugin;
+const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require("path");
 const { dependencies, port, name } = require("./package.json");
@@ -64,7 +64,7 @@ module.exports = (env = { ssoPort: 3001, navPort: 3003 }, argv) => {
     },
     plugins: [
       new MiniCssExtractPlugin(),
-      new ModuleFederationPlugin({
+      new webpack.container.ModuleFederationPlugin({
         name,
         filename: "remoteEntry.js",
         remotes: {
